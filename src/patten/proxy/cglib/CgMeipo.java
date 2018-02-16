14d6494725d07564705c165252cbed7ce6121841
邓天骄 <jiao_snow@126.com>
2018-02-16 14:39:03 +0800

    cglib动态代理

--- src/patten/proxy/cglib/CgMeipo.java
+++ src/patten/proxy/cglib/CgMeipo.java
@@ -16,6 +16,7 @@ public class CgMeipo implements MethodInterceptor {
     public Object getInstance(String className)throws Exception{
         Enhancer enhancer = new Enhancer();
         Class clazz = Class.forName(className);
+        //这一步就是告诉cglib，生成的子类需要继承哪个类，只需要一个类名就可以类，不需要传入这个类的对象
         enhancer.setSuperclass(clazz);
         enhancer.setCallback(this);
         return enhancer.create();
@@ -30,6 +31,12 @@ public class CgMeipo implements MethodInterceptor {
 //        outputStream.write(data);
 //        outputStream.close();
         System.out.println("开始寻找");
+        //这个Object的引用是由CGLib给我们new出来的
+        //cglib new出来以后的对象，是被代理对象的子类（继承了我们自己写的那个类）
+        //OOP, 在new子类之前，实际上默认先调用了我们super()方法的，
+        //new了子类的同时，必须先new出来父类，这就相当于是间接的持有了我们父类的引用
+        //子类重写了父类的所有的方法
+        //我们改变子类对象的某些属性，是可以间接的操作父类的属性的
         methodProxy.invokeSuper(o,objects);
         System.out.println("找到个匹配的");
         return null;
